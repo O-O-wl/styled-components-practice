64df9c5a48eb3112efeaa7b0047ed479d05703d1
ldcpaul <ldcpaul@hanmail.net>
2018-08-23 02:51:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from './Button/Button'
+
+
+class App extends React.Component{
+render(){
+    
+    return(
+    <div>
+        <Button >버튼</Button>
+        </div> 
+
+
+    )
+}
+
+
+
+
+}
+export default App;
